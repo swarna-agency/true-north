00bf988e89139634e0487ea784353a77ed667182
Gita Ventyana <gita.ventyana@gmail.com>
2024-03-11 23:33:26 +0700

    Preload images

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import type { MetaFunction } from "@remix-run/node";
-// import { AudioPlayer } from "~/components/audio-player";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Opening } from "~/components/opening";
 import { MainPage } from "~/components/main-page";
 import { useState } from "react";
@@ -10,6 +9,61 @@ export const meta: MetaFunction = () => {
     {
       name: "description",
       content: `Check out The True North, a deeply personal reflection and musical journey, drawing inspiration and sustenance from the living colours of Australia’s natural environment and the sights and sounds of modern Oz.`,
+    },
+  ];
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "preload",
+      href: "/images/bg/hero-bg.jpeg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/01-img.jpg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/02-img.jpg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/03-img.jpeg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/04-img.jpeg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/05-img.jpg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/06-img.jpeg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/07-img.jpeg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/08-img.jpg",
+      as: "image",
+    },
+    {
+      rel: "preload",
+      href: "/images/player/09-img.jpeg",
+      as: "image",
     },
   ];
 };
