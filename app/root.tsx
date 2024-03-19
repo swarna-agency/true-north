0cba065cddbc605678a1ec4a83299320e4f88501
Gita Ventyana <gita.ventyana@gmail.com>
2024-03-19 11:50:06 +0700

    GTM tag

--- app/root.tsx
+++ app/root.tsx
@@ -59,12 +59,30 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KXDJHVF6');`,
+          }}
+        ></script>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
+        <noscript>
+          <iframe
+            title="GTM"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KXDJHVF6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
